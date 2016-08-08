28fc11753c090b7f1685dd1f379dcdbaa9cc745a
Broenne <marcus.broenstrup@gmail.com>
2016-08-08 19:24:12 +0200

    no message

---
+++ wait.bat
@@ -0,0 +1,9 @@
+echo "waiting 2 seconds ..."
+PING 1.1.1.1 -n 1 -w 2000 >NUL
+echo "waiting 2 seconds ..."
+PING 1.1.1.1 -n 1 -w 2000 >NUL
+echo "waiting 2 seconds ..."
+PING 1.1.1.1 -n 1 -w 2000 >NUL
+echo "waiting 2 seconds ..."
+PING 1.1.1.1 -n 1 -w 2000 >NUL
+echo "DONE!"
